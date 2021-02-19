9a19e174412f1fc1b2e834be45eee8a1d50f247e
palepanorama <matthew.keith.thomas@gmail.com>
2021-02-19 16:52:04 -0600

    commit tree f27d2af901d3afe2c5f331632f7e024576bc1054

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainId = document.getElementById('main');
+mainId.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Matt is the champion";
